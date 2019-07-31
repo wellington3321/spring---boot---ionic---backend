d6ddcc3e9ca517e5c7275e82a7a056c7b86fac69
wellington jose da silva <wdark@hotmail.com>
2019-07-31 15:41:31 -0300

    Endpoint/clientes/{id}

---
+++ src/main/java/com/ligia/cursomc/services/ClienteService.java
@@ -0,0 +1,27 @@
+package com.ligia.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ligia.cursomc.domain.Cliente;
+import com.ligia.cursomc.repositories.ClienteRepository;
+import com.ligia.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		
+		
+	}
+	
+
+}
