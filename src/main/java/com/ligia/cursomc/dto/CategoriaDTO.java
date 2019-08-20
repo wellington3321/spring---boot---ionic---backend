6723da26495d0bc9fdb60a07339d92fb2cb768d6
wellington jose da silva <wdark@hotmail.com>
2019-08-20 16:24:59 -0300

    Validacao statica com Bean Validation

--- src/main/java/com/ligia/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/ligia/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,19 @@ package com.ligia.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.ligia.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento obrigatorio")
+	@Length(min=5, max=80, message = "O tamanho deve ser entre 5 a 80 caracters")
 	private String nome;
 	
 	public CategoriaDTO() {
