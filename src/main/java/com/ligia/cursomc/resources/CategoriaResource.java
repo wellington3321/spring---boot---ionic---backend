a6a91dfe65b29b1232dfca528da915280da43ff7
wellington jose da silva <wdark@hotmail.com>
2019-08-20 10:30:31 -0300

    Inserindo uma nova Categoria

--- src/main/java/com/ligia/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ligia/cursomc/resources/CategoriaResource.java
@@ -40,6 +40,7 @@ public class CategoriaResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+		//eu
 	}
 
 }
