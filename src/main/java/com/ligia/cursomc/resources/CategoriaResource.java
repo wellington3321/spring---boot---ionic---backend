7cd8b862905a11473ceea8d6b1f91b23eb31959a
wellington jose da silva <wdark@hotmail.com>
2019-08-20 12:17:47 -0300

    Listando todas as Categorias com DTO

--- src/main/java/com/ligia/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ligia/cursomc/resources/CategoriaResource.java
@@ -3,8 +3,8 @@ package com.ligia.cursomc.resources;
 
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ligia.cursomc.domain.Categoria;
+import com.ligia.cursomc.dto.CategoriaDTO;
 import com.ligia.cursomc.services.CategoriaService;
 
 @RestController
@@ -57,5 +58,11 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	 }
 
 }
--- src/main/java/com/ligia/cursomc/services/CategoriaService.java
+++ src/main/java/com/ligia/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.ligia.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +45,11 @@ public class CategoriaService {
 			throw new DataIntegrityException("Nao e possivel excluir uma categoria que possui produtos");
 		
 		}
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+		
 	}
 
 }
