ac39053b04eb76e2488711d6a81ccf7243bb9e67
wellington jose da silva <wdark@hotmail.com>
2019-08-20 17:07:03 -0300

    Personalizando o retorno da Validacao

---
+++ src/main/java/com/ligia/cursomc/resources/exception/ValidationError.java
@@ -0,0 +1,27 @@
+package com.ligia.cursomc.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.bytebuddy.implementation.FieldAccessor.FieldNameExtractor;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+
+
+
+}
